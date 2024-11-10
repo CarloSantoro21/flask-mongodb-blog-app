a7c9cbfa63724b434ce49a94b2e7247fc022363e
CarloSantoro21 <alessandrosantoro212121@gmail.com>
2024-11-10 01:03:10 -0600

    BETAv4.5

--- app.py
+++ app.py
@@ -146,5 +146,5 @@ def logout():
     return redirect(url_for('index'))
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
